cf6c3b2a9aa4f5b05a702816a8baf445934c9010
robinsonkwame <kporterrobinson@gmail.com>
2019-08-21 14:19:30 -0400

    (feat) adds first iteration of adversarial labelling. Note that the results sans PassengerID are poor, the basic preprocessor removes a lot and results seem basically the same throughout; I know it's working since it works great with PassenginerID in there, so this is really a question of preprocessing, model building. Maybe try RandomForest? Maybe move on to using Vtreat (shouldn't be a dependency though)

--- adversarial_labeller.py
+++ adversarial_labeller.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 import pandas as pd
 from sklearn.base import TransformerMixin
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.preprocessing import FunctionTransformer
 
 def read_concat_and_label_test_train_data(test_filename="test.csv",
                                           train_filename="train.csv",
                                           data_dir="./test/fixtures",
                                           y_columns=['Survived'],
-                                          label_column="label"):
+                                          label_column="label",
+                                          drop_columns=["PassengerId"]):
     drop_args = {
         "axis":"columns",
         "inplace": True
@@ -28,28 +30,38 @@ def read_concat_and_label_test_train_data(test_filename="test.csv",
                 test_df),
             **concat_args
         )
-    df.drop(y_columns, **drop_args)
+    df.drop(y_columns + drop_columns, **drop_args)
     df[label_column] = 0
     df.loc[len(test_df):, label_column] = 1
     return df
 
 def get_variable_and_label_columns(df, label_column="label"):
-    return df.drop(label_column, axis='column'), df[label_column] 
+    return df.drop(label_column, axis='columns'), df[label_column] 
 
-class AdversarialClassifier(LogisticRegression):
-    # note:  if we wanted this to be dynamic we'd use dependency injection
+def keep_numeric_columns_only_replace_na(df: pd.DataFrame):
+    return df.select_dtypes(include='number')\
+             .fillna(value=-1, axis="columns")
+
+MyAdversarialPreprocessor = FunctionTransformer(
+    keep_numeric_columns_only_replace_na,
+    check_inverse=False,
+    validate=False)
+
+class MyAdversarialClassifier(LogisticRegression):
     pass
 
-class AdversarialLabeller(TransformerMixin, AdversarialClassifier):
+class AdversarialLabeller(MyAdversarialClassifier, TransformerMixin):
     def __init__(self):
-        pass
+        super().__init__()        
 
     def transform(self, X):
         return self.predict_proba(X)
 
-#todo: make sure this works as expected:
-# 
-# can instantiate an AdversarialLabeller with, say,
-# a randomforest, use fit with appropriately transformed data
-# then can use transform and get probabilities, would be pretty cool
-# if this simple
+if __name__ == "__main__":
+    df = read_concat_and_label_test_train_data()
+    variables, labels = get_variable_and_label_columns(df)
+    preprocessed_values = MyAdversarialPreprocessor.transform(variables)
+    adversarial_labeller = AdversarialLabeller()
+    adversarial_labeller.fit(preprocessed_values.values, labels.values)
+    # e.g. ...
+    #   adversarial_labeller.transform(preprocessed_values.values)
