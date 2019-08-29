f0f826430736dfae11a0d6be3e7f5121a9b371ad
robinsonkwame <kporterrobinson@gmail.com>
2019-08-29 13:31:07 -0400

    (WIP) Includes RF randomized CV for hyper parameter search for predict test label problem. I'm seeing near 0 F1 score for not in test set, essentially making it predict 1 for everything. That said, the F1 score for predicting in test set is 0.80, with a precision of almost 70%
    todo: re run with weight test score; both are important; I think I'd be okay with a higher precision and lower recall, however, for predicting in test samples. Note sure

--- adversarial_labeller.py
+++ adversarial_labeller.py
@@ -1,14 +1,21 @@
+import numpy as np
 from sklearn.base import TransformerMixin
-from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.linear_model.logistic import LogisticRegression, LogisticRegressionCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 
+
 class AdversarialRandomForestLabeller(RandomForestClassifier, TransformerMixin):
-    def __init__(self, args_for_randomforest):
-        super().__init__(**args_for_randomforest)
+    def __init__(self, fit_params=None):
+        super().__init__(**fit_params)
+        self.params = fit_params
 
     def transform(self, X):
         return self.predict_proba(X)
+    
+    def get_params(self, deep=False):
+        return self.params
 
 class AdversarialExtraTreesLabeller(ExtraTreesClassifier, TransformerMixin):
     def __init__(self, args_for_classifer):
@@ -23,3 +30,65 @@ class AdversarialLogisticRegressionLabeller(LogisticRegression, TransformerMixin
 
     def transform(self, X):
         return self.predict_proba(X)
+
+class AdversarialLogisticRegressionCVLabeller(LogisticRegressionCV, TransformerMixin):
+    def __init__(self, random_state, cv, solver, class_weight, penalty):
+        super().__init__(
+            penalty=penalty,
+            class_weight=class_weight,
+            random_state=random_state,
+            cv=cv,
+            solver=solver)
+
+    def transform(self, X):
+        return self.predict_proba(X)
+
+    def get_params(self, deep=False):
+        return {
+            'random-state': self.random_state,
+            'cv': self.cv
+        }
+
+class RandomForestRandomizedCV:
+    # from https://towardsdatascience.com/hyperparameter-tuning-the-random-forest-in-python-using-scikit-learn-28d2aa77dd74
+    n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+    max_features = ['auto', 'sqrt']
+    max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+    max_depth.append(None)
+    min_samples_split = [2, 5, 10]
+    min_samples_leaf = [1, 2, 4]
+    bootstrap = [True, False]
+    class_weight=[None, "balanced", "balanced_subsample"]
+
+    random_grid = {'n_estimators': n_estimators,
+                   'max_features': max_features,
+                   'max_depth': max_depth,
+                   'min_samples_split': min_samples_split,
+                   'min_samples_leaf': min_samples_leaf,
+                   'bootstrap': bootstrap,
+                   'class_weight': class_weight}
+    
+    def get_best_parameters(self,
+                            features,
+                            labels,
+                            n_iter=100,
+                            cv=3,
+                            verbose=2,
+                            random_state=1,
+                            n_jobs=-1):
+        rf_random =\
+            RandomizedSearchCV(
+                estimator=RandomForestClassifier(),
+                param_distributions=self.random_grid,
+                n_iter=n_iter,
+                cv=cv,
+                verbose=verbose,
+                random_state=random_state,
+                n_jobs=n_jobs
+            )
+        rf_random.fit(
+            features,
+            labels
+        )
+
+        return rf_random.best_params_
--- adversarial_labeller2.py
+++ adversarial_labeller2.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 import pandas as pd
-from adversarial_labeller import AdversarialRandomForestLabeller, AdversarialExtraTreesLabeller
+from adversarial_labeller import (
+    AdversarialRandomForestLabeller,
+    AdversarialLogisticRegressionCVLabeller,
+    RandomForestRandomizedCV
+    )
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.feature_extraction.text import CountVectorizer
@@ -49,206 +53,307 @@ MyBasicAdversarialPreprocessor = FunctionTransformer(
     check_inverse=False,
     validate=False)
 
+def get_test_train_samples(df,
+                           sample_args=None):
+    if not sample_args:
+        sample_args =\
+            {
+                "frac": 0.85,
+                "random_state": 1
+            }
+    train_df = df.sample(**sample_args)
+    test_df = df.drop(train_df.index)
+
+    return train_df, test_df  # should have an API manager?
+
 if __name__ == "__main__":
-    from sklearn.metrics import roc_auc_score
-    sample_args = {
-        "frac": 0.85,
-        "random_state": 1
-    }
+    from sklearn.model_selection import ShuffleSplit, StratifiedShuffleSplit   
+    from sklearn.metrics import roc_auc_score, accuracy_score, classification_report
+    from sklearn.model_selection import cross_val_score
 
     df = read_concat_and_label_test_train_data()
     variables, labels = get_variable_and_label_columns(df)
+    preprocessed_values =\
+        MyBasicAdversarialPreprocessor.transform(
+            variables
+        )
+    test_df, train_df = get_test_train_samples(
+        preprocessed_values
+    )
 
-    # # vectorizer 
-    # vectorizer = CountVectorizer()
-    # extra_features = vectorizer.fit_transform(
-    #     df.Name.str.replace(",", "_is_a_last_name")
+    # adversarial_labeller =\
+    #     AdversarialLogisticRegressionCVLabeller(
+    #         penalty='l2',
+    #         class_weight='balanced',
+    #         solver='liblinear',
+    #         random_state=1,
+    #         cv=5).fit(
+    #     train_df.values,
+    #     labels[train_df.index].values
     # )
 
-    preprocessed_values = MyBasicAdversarialPreprocessor.transform(variables)
+    # print(
+    #     "Accuracy: %0.2f (+/- %0.2f)" % (
+    #         adversarial_labeller.scores_[1].mean(),
+    #         adversarial_labeller.scores_[1].std() * 2)
+    #     )
+    # print(
+    #     "Validation Accuracy: %0.2f" % (
+    #         accuracy_score(
+    #             labels[test_df.index],
+    #             adversarial_labeller.predict(
+    #                 test_df.values
+    #             )
+    #         )
+    #     )
+    # )
+    # print(
+    #     classification_report(
+    #         y_true= labels[test_df.index],
+    #         y_pred= adversarial_labeller.predict(
+    #                     test_df.values
+    #                 )
+    #     )
+    # )
 
-    train_df = preprocessed_values.sample(**sample_args)
-    test_df = preprocessed_values.drop(train_df.index)
+    # adversarial_labeller = AdversarialRandomForestLabeller(
+    #     n_estimators=50
+    # )
+
+    # cross_validator =\
+    #     StratifiedShuffleSplit(n_splits=5, test_size=0.3, random_state=1)
+    # scores =\
+    #     cross_val_score(adversarial_labeller,
+    #                     train_df.values,
+    #                     labels[train_df.index].values,
+    #                     cv=cross_validator)
+
+    fit_params =\
+        RandomForestRandomizedCV().get_best_parameters(
+            features=train_df.values,
+            labels=labels[train_df.index]
+        )
 
     adversarial_labeller = AdversarialRandomForestLabeller(
-        args_for_randomforest={
-            "n_estimators":100
-        }
+        fit_params=fit_params
     )
+
     adversarial_labeller.fit(
         train_df.values,
         labels[train_df.index].values
     )
-    y_score = adversarial_labeller.transform(test_df.values)
-    roc_auc_score(
-        labels[test_df.index].values,
-        y_score[:,1]
-    )
-    # Out[136]: 0.5286266924564796
-
-    # try with vtreat as the preprocessor
-    import vtreat
-
-    plan = vtreat.BinomialOutcomeTreatment(outcome_name="label",
-                                           outcome_target=True)
-
-    train_df = variables.sample(**sample_args)
-    test_df = variables.drop(train_df.index)
-
-    cross_frame = plan.fit_transform(
-        train_df,
-        labels[train_df.index].values
-    )
-    adversarial_labeller = AdversarialRandomForestLabeller(
-        args_for_randomforest={
-            "n_estimators":100
-        }
-    )        
-
-    adversarial_labeller = AdversarialExtraTreesLabeller(
-        args_for_classifer={
-            "n_estimators":100
-        }
-    )
-
-    adversarial_labeller.fit(
-        cross_frame.values,
-        labels[train_df.index].values
-    )
-
-    y_score = adversarial_labeller.transform(
-        plan.transform(test_df)
-    )
-    roc_auc_score(
-        labels[test_df.index].values,
-        y_score[:, 1]
-    )
-    #Out[123]: 0.5499032882011605
-
-    #  Need to create adversarial classifier by
-    # classifier(Classifier)
-    # def score(X, y, sample_weight):
-    #   and we can assign a sample weight via probability 
-    # instance is a test instnace.
-
-    # this might not be well founded, but can be checked
-    # against Kaggle submission (e.g., score should be similar)
-
-    # Okay, so I think I can 
-    #   use fit_params in a CV like thing
-    # and also provide sample_weight as well as score_params
-
-    df =\
-        pd.read_csv('./test/fixtures/train.csv')\
-          .drop("PassengerId", axis="columns")
-    preprocessed_df = MyBasicAdversarialPreprocessor.transform(df)
-
-    variables = preprocessed_df.drop("Survived", axis="columns")
-    labels = preprocessed_df["Survived"]
-
-    rf_train_df = variables.sample(**sample_args)
-    rf_test_df = variables.drop(rf_train_df.index)
-
-    new_cross_frame =\
-        plan.transform(
-            df.loc[rf_train_df.index]
-        )
-
-    rf = RandomForestClassifier()
-    rf.fit(rf_train_df.values,
-           labels[rf_train_df.index],
-           sample_weight=\
-               adversarial_labeller.transform(
-                new_cross_frame)[:, 1]
-    )
-
-    rf2 = RandomForestClassifier()
-    rf2.fit(rf_train_df.values,
-            labels[rf_train_df.index]
-    )
-
-    test_cross_frame =\
-        plan.transform(
-            df.loc[rf_test_df.index]
-        )
-    sample_weights =\
-        adversarial_labeller.transform(
-            test_cross_frame)[:, 1]
-
-    #  Test on hold out set
-    from sklearn.metrics import accuracy_score
-    print(
-        accuracy_score(labels[rf_test_df.index],
-                       rf.predict(rf_test_df.values),
-                       sample_weight=\
-                        adversarial_labeller.transform(
-                            test_cross_frame)[:, 1]
-        )
-    )
 
     print(
-        accuracy_score(survived[test_df.index],
-                       rf.predict(
-                           plan.transform(test_df))
+        "Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+    print(
+        "Validation Accuracy: %0.2f" % (
+            accuracy_score(
+                labels[test_df.index],
+                adversarial_labeller.predict(
+                    test_df.values
+                )
+            )
+        )
+    )
+    print(
+        classification_report(
+            y_true= labels[test_df.index],
+            y_pred= adversarial_labeller.predict(
+                        test_df.values
+                    )
         )
     )
 
-    from sklearn.metrics import accuracy_score
-    print(
-        accuracy_score(labels[rf_test_df.index].values,
-                       rf2.predict(rf_test_df.values),
-                       sample_weight=sample_weights
+    # adversarial_labeller.fit(
+    #     train_df.values,
+    #     labels[train_df.index].values
+    # )
+
+    # rf = RandomForestClassifier()
+    # rf.fit(train_df.values,
+    #        labels[train_df.index])
+
+    # # ----
+    
+    # # # vectorizer 
+    # # vectorizer = CountVectorizer()
+    # # extra_features = vectorizer.fit_transform(
+    # #     df.Name.str.replace(",", "_is_a_last_name")
+    # # )
+
+    # preprocessed_values = MyBasicAdversarialPreprocessor.transform(variables)
+
+    # y_score = adversarial_labeller.transform(test_df.values)
+    # roc_auc_score(
+    #     labels[test_df.index].values,
+    #     y_score[:,1]
+    # )
+    # # Out[136]: 0.5286266924564796
+
+    # # try with vtreat as the preprocessor
+    # import vtreat
+
+    # plan = vtreat.BinomialOutcomeTreatment(outcome_name="label",
+    #                                        outcome_target=True)
+
+    # train_df = variables.sample(**sample_args)
+    # test_df = variables.drop(train_df.index)
+
+    # cross_frame = plan.fit_transform(
+    #     train_df,
+    #     labels[train_df.index].values
+    # )
+    # adversarial_labeller = AdversarialRandomForestLabeller(
+    #     args_for_randomforest={
+    #         "n_estimators":100
+    #     }
+    # )        
+
+    # adversarial_labeller = AdversarialExtraTreesLabeller(
+    #     args_for_classifer={
+    #         "n_estimators":100
+    #     }
+    # )
+
+    # adversarial_labeller.fit(
+    #     cross_frame.values,
+    #     labels[train_df.index].values
+    # )
+
+    # y_score = adversarial_labeller.transform(
+    #     plan.transform(test_df)
+    # )
+    # roc_auc_score(
+    #     labels[test_df.index].values,
+    #     y_score[:, 1]
+    # )
+    # #Out[123]: 0.5499032882011605
+
+    # #  Need to create adversarial classifier by
+    # # classifier(Classifier)
+    # # def score(X, y, sample_weight):
+    # #   and we can assign a sample weight via probability 
+    # # instance is a test instnace.
+
+    # # this might not be well founded, but can be checked
+    # # against Kaggle submission (e.g., score should be similar)
+
+    # # Okay, so I think I can 
+    # #   use fit_params in a CV like thing
+    # # and also provide sample_weight as well as score_params
+
+    # df =\
+    #     pd.read_csv('./test/fixtures/train.csv')\
+    #       .drop("PassengerId", axis="columns")
+    # preprocessed_df = MyBasicAdversarialPreprocessor.transform(df)
+
+    # variables = preprocessed_df.drop("Survived", axis="columns")
+    # labels = preprocessed_df["Survived"]
+
+    # rf_train_df = variables.sample(**sample_args)
+    # rf_test_df = variables.drop(rf_train_df.index)
+
+    # new_cross_frame =\
+    #     plan.transform(
+    #         df.loc[rf_train_df.index]
+    #     )
+
+    # rf = RandomForestClassifier()
+    # rf.fit(rf_train_df.values,
+    #        labels[rf_train_df.index],
+    #        sample_weight=\
+    #            adversarial_labeller.transform(
+    #             new_cross_frame)[:, 1]
+    # )
+
+    # rf2 = RandomForestClassifier()
+    # rf2.fit(rf_train_df.values,
+    #         labels[rf_train_df.index]
+    # )
+
+    # test_cross_frame =\
+    #     plan.transform(
+    #         df.loc[rf_test_df.index]
+    #     )
+    # sample_weights =\
+    #     adversarial_labeller.transform(
+    #         test_cross_frame)[:, 1]
+
+    # #  Test on hold out set
+    # from sklearn.metrics import accuracy_score
+    # print(
+    #     accuracy_score(labels[rf_test_df.index],
+    #                    rf.predict(rf_test_df.values),
+    #                    sample_weight=\
+    #                     adversarial_labeller.transform(
+    #                         test_cross_frame)[:, 1]
+    #     )
+    # )
+
+    # print(
+    #     accuracy_score(survived[test_df.index],
+    #                    rf.predict(
+    #                        plan.transform(test_df))
+    #     )
+    # )
+
+    # from sklearn.metrics import accuracy_score
+    # print(
+    #     accuracy_score(labels[rf_test_df.index].values,
+    #                    rf2.predict(rf_test_df.values),
+    #                    sample_weight=sample_weights
                         
-        )
-    )
-    # Gets .763 (higher)
+    #     )
+    # )
+    # # Gets .763 (higher)
 
-    print(
-        accuracy_score(labels[rf_test_df.index].values,
-                       rf2.predict(rf_test_df.values)
-        )
-    )
-    # Gets .76 on hold out
-    # Get 0.64593 on test
+    # print(
+    #     accuracy_score(labels[rf_test_df.index].values,
+    #                    rf2.predict(rf_test_df.values)
+    #     )
+    # )
+    # # Gets .76 on hold out
+    # # Get 0.64593 on test
+
+    # mask  = sample_weights == 1
+    # print(
+    #     accuracy_score(labels[rf_test_df.index][mask].values,
+    #                    rf2.predict(rf_test_df[mask].values)
+    #     )
+    # )
+    # # Gets 0.6875 on score check (0.04 off from test)
+
+
+    # titanic_df =\
+    #     pd.read_csv('./test/fixtures/test.csv')\
+    #       .drop("PassengerId", axis="columns")
+    # titanic_df.index += 892
+    # preprocessed_titanic_df = MyBasicAdversarialPreprocessor.transform(titanic_df)
+
+
+    # predictions =\
+    #     rf.predict(preprocessed_titanic_df)
     
-    mask  = sample_weights == 1
-    print(
-        accuracy_score(labels[rf_test_df.index][mask].values,
-                       rf2.predict(rf_test_df[mask].values)
-        )
-    )
-    # Gets 0.6875 on score check (0.04 off from test)
-
-
-    titanic_df =\
-        pd.read_csv('./test/fixtures/test.csv')\
-          .drop("PassengerId", axis="columns")
-    titanic_df.index += 892
-    preprocessed_titanic_df = MyBasicAdversarialPreprocessor.transform(titanic_df)
-
-
-    predictions =\
-        rf.predict(preprocessed_titanic_df)
+    # outputs =\
+    #     pd.DataFrame(
+    #         {'PassengerId': preprocessed_titanic_df.index,
+    #          'Survived': predictions}
+    #     )
+    # outputs.to_csv('titanic_output.csv', index=False)
+    # # gets 0.333 on kaggle, against label for test instance or not
     
-    outputs =\
-        pd.DataFrame(
-            {'PassengerId': preprocessed_titanic_df.index,
-             'Survived': predictions}
-        )
-    outputs.to_csv('titanic_output.csv', index=False)
-    # gets 0.333 on kaggle, against label for test instance or not
-    
-    # rewrote to be against train.csv, with vtreat sample weights
-    # under fit
-    # gets 0.59808
+    # # rewrote to be against train.csv, with vtreat sample weights
+    # # under fit
+    # # gets 0.59808
 
-    predictions2 =\
-        rf2.predict(preprocessed_titanic_df)
+    # predictions2 =\
+    #     rf2.predict(preprocessed_titanic_df)
     
-    outputs2 =\
-        pd.DataFrame(
-            {'PassengerId': preprocessed_titanic_df.index,
-             'Survived': predictions2}
-        )
-    outputs2.to_csv('titanic_output2.csv', index=False)
-    #  gets 0.40191 on kaggle
+    # outputs2 =\
+    #     pd.DataFrame(
+    #         {'PassengerId': preprocessed_titanic_df.index,
+    #          'Survived': predictions2}
+    #     )
+    # outputs2.to_csv('titanic_output2.csv', index=False)
+    # #  gets 0.40191 on kaggle
